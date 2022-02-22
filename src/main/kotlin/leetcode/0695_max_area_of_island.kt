785b8dde071a4adca7d1574120ce75921045ea1f
chenweida <chenweida@mistong.com>
2022-02-22 16:11:40 +0800

    0695 最大岛屿 使用栈和队列实现dfs和bfs

--- src/main/kotlin/leetcode/0695_max_area_of_island.kt
+++ src/main/kotlin/leetcode/0695_max_area_of_island.kt
@@ -1,25 +1,30 @@
 package leetcode
 
+import java.util.*
+
 //【树】【DFS】【BFS】
 class _0695 {
 
     fun exec(args: Array<IntArray>): Int {
-        return Solution().maxAreaOfIsland(args)
+        return Solution().maxAreaOfIsland2(args)
     }
 
     class Solution {
         fun maxAreaOfIsland(grid: Array<IntArray>): Int {
             if (grid.isEmpty()) return 0
+            val target = Array<IntArray>(grid.size) { intArrayOf() }
+            // 拷贝一份grid，不要修改原始数组
+            grid.copyInto(target)
             var result = 0
-            val maxRow = grid.size
-            val maxColumn = grid[0].size
+            val maxRow = target.size
+            val maxColumn = target[0].size
             for (i in 0 until maxRow) {
                 for (j in 0 until maxColumn) {
-                    if (grid[i][j] == 0) {
+                    if (target[i][j] == 0) {
                         continue
                     }
-                    val single = dfs(grid, i, j)
-                    result = if (single > result) single else result
+                    val isle = dfs(target, i, j)
+                    result = if (isle > result) isle else result
                 }
             }
             return result
@@ -39,6 +44,70 @@ class _0695 {
             result += dfs(grid, row-1, column)
             result += dfs(grid, row, column+1)
             result += dfs(grid, row, column-1)
+            return result
+        }
+
+        fun maxAreaOfIsland2(grid: Array<IntArray>): Int {
+            if (grid.isEmpty()) return 0
+            val target = Array<IntArray>(grid.size) { intArrayOf() }
+            // 拷贝一份grid，不要修改原始数组
+            grid.copyInto(target)
+            var result = 0
+            val maxRow = target.size
+            val maxColumn = target[0].size
+            val stack = Stack<IntArray>()
+            val queue = ArrayDeque<IntArray>(100) // arrayqueue基于数组，需要给定一个capacity
+            for (i in 0 until maxRow) {
+                for (j in 0 until maxColumn) {
+//                    stack.push(intArrayOf(i, j))
+//                    val isle = dfs2(grid, stack)
+                    queue.offer(intArrayOf(i, j))
+                    val isle = bfs(grid, queue)
+                    result = if (isle > result) isle else result
+                }
+            }
+            return result
+        }
+
+        fun bfs(grid: Array<IntArray>, queue: Queue<IntArray>): Int {
+            var result = 0
+            while (queue.isNotEmpty()) {
+                val pair = queue.remove()
+                val row = pair?.getOrNull(0) ?: continue
+                val column = pair.getOrNull(1) ?: continue
+                if (row >= grid.size || column >= grid[0].size || row < 0 || column < 0 || grid[row][column] != 1) {
+                    continue
+                }
+                grid[row][column] = 0
+                result++
+                // 将四个方位append到队列中, left - top - right - bottom
+                // 会先遍历完一个节点的子节点
+                queue.offer(intArrayOf(row-1, column))
+                queue.offer(intArrayOf(row, column-1))
+                queue.offer(intArrayOf(row+1, column))
+                queue.offer(intArrayOf(row, column+1))
+            }
+            return result
+        }
+
+        fun dfs2(grid: Array<IntArray>, stack: Stack<IntArray>): Int {
+            var result = 0
+            while (stack.isNotEmpty()) {
+                val pair = stack.pop()
+                val row = pair?.getOrNull(0) ?: continue
+                val column = pair.getOrNull(1) ?: continue
+                if (row >= grid.size || column >= grid[0].size || row < 0 || column < 0 || grid[row][column] != 1) {
+                    continue
+                }
+                grid[row][column] = 0
+                result++
+                // 将四个方位压入栈中, left - top - right - bottom
+                // 会从左边的节点遍历到该方向的最深处
+                stack.push(intArrayOf(row-1, column))
+                stack.push(intArrayOf(row, column-1))
+                stack.push(intArrayOf(row+1, column))
+                stack.push(intArrayOf(row, column+1))
+            }
             return result
         }
     }
