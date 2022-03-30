84bd95c4fc97857addd59b5264669911633dea63
chenweida <chenweida@mistong.com>
2022-03-30 15:01:55 +0800

    0543 二叉树直径，递归：左子树深度+右子树深度

---
+++ src/main/kotlin/leetcode/0543_diameter_of_binary_tree.kt
@@ -0,0 +1,26 @@
+package leetcode
+
+class _0543 {
+
+    // 一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点
+    class Solution {
+
+        // 每个节点的直径：最长左子树路径 + 最长右子树路径 + 1
+        fun diameterOfBinaryTree(root: TreeNode?): Int {
+            depth(root)
+            return result
+        }
+
+        var result = 0
+        fun depth(root: TreeNode?): Int {
+            if (root == null) {
+                return 0
+            }
+            val left = depth(root.left) // 左子树最大深度
+            val right = depth(root.right) // 右子树最大深度
+            // 直径 = left + right
+            result = maxOf(left + right, result)
+            return maxOf(left, right) + 1
+        }
+    }
+}
